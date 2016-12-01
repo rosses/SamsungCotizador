81b3e6e06a4be783a4b27cc9796f05f0fd94f095
Roberto Osses <rosses@gmail.com>
2016-12-01 03:55:16 -0300

    start

---
+++ www/js/app.js
@@ -0,0 +1,56 @@
+app = {
+        restApi:  "http://services.samsungstore.cl:8085/?",
+        impID: "",
+        impNN: "",
+        impSERV: "18F0",
+        impCHAR: "2AF1",
+        auth: 0
+      };
+var printers = [];
+
+//http://services.samsungstore.cl:8085/?action=Maestra&store_code=002
+angular.module('samsungcot', ['ionic', 'samsungcot.controllers','ngStorage'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('home', {
+    url: '/home',
+    templateUrl: 'templates/home.html',
+    controller: 'HomeCtrl'
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  }) 
+
+  .state('home.main', {
+    url: '/main',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/main.html',
+        controller: 'MainCtrl'
+      }
+    }
+  })
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,189 @@
+
+function err(msg) {
+  console.log(msg);
+  navigator.notification.alert(
+      (msg ? msg : 'Error al consultar el servicio. Intente más tarde'),  // message
+      function() { },
+      'Error',
+      'OK'
+  );
+}
+
+String.prototype.toBytes = function() {
+    var arr = []
+    for (var i=0; i < this.length; i++) {
+    arr.push(this[i].charCodeAt(0))
+    }
+    return arr
+}
+
+angular.module('samsungcot.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $ionicSideMenuDelegate, $ionicPopup, $ionicLoading) {
+
+
+
+})
+
+.controller('HomeCtrl', function($scope, $state, $localStorage, $location) {
+  
+  if (!$localStorage.app) { $localStorage.app = app; }
+  if ($localStorage.app.auth == 0) {
+      $location.path( "login", false );
+  }
+
+  $scope.printerbox = {};
+  $scope.cargandoPrinters = true;
+  $scope.noPrinterFound = false;
+
+  printers = [];
+  ble.startScan([], function(device) {
+    printer = { nombre: device.name, id: device.id };
+    printers.push(printer);
+    var string = device.name,
+    substring = "SAMSTORECC";
+    if (string.indexOf(substring) !== -1) {
+      app.impID = device.id;
+      app.impNN = device.name;
+      $localStorage.app = app;
+    }
+  } , function() {
+    // ERR
+  });
+
+  $timeout(function() {
+
+    $scope.cargandoPrinters = false;
+    $scope.printerList=printers;
+
+    if (printers.length == 0) {
+      $scope.noPrinterFound = true;
+    }
+
+    ble.stopScan(function() {}, function() {});
+
+  },5000)
+
+  $scope.imprimir = function() {
+    if (app.impNN != "") {
+      var buffer = new MutableBuffer(/* initialSize, blockSize */);
+
+      var code = "00015742";
+      var type = "CODE128";
+      var width = "200";
+      var height = "4";
+      var position = "BLW";
+      var font = "A";
+      if(width >= 1 || width <= 255){
+        buffer.write(_.BARCODE_FORMAT.BARCODE_WIDTH);
+      }
+      if(height >=2  || height <= 6){
+        buffer.write(_.BARCODE_FORMAT.BARCODE_HEIGHT);
+      }
+      buffer.write(_.BARCODE_FORMAT[
+        'BARCODE_FONT_' + (font || 'A').toUpperCase()
+      ]);
+      buffer.write(_.BARCODE_FORMAT[
+        'BARCODE_TXT_' + (position || 'BLW').toUpperCase()
+      ]);
+      buffer.write(_.BARCODE_FORMAT[
+        'BARCODE_' + ((type || 'EAN13').replace('-', '_').toUpperCase())
+      ]);
+      buffer.write(code);
+
+      ble.writeWithoutResponse(app.impID, app.impSERV, app.impCHAR, buffer, function(x) { 
+        err('OK'); 
+      }, function(x) { 
+        err('No se pudo imprimir');
+      });
+    }
+    else {
+      err('No se ha configurado una impresora');
+    }
+  };
+
+
+  $scope.impActivar = function(item) {
+
+    if (app.impID != null) {
+      ble.isConnected(amp.impID, function() {
+        ble.disconnect(amp.impID, function() {}, function() {});
+      }, function() {
+
+      });
+    }
+
+    app.impNN = item.currentTarget.getAttribute("data-nombre");
+    app.impID = $scope.printerbox.sel;
+    $localStorage.app = app;
+
+    ble.connect(app.impID, function(peripheral) {
+      $location.path( "main/home", false ); 
+      $ionicLoading.hide();
+    }, function() { 
+      err('Problemas al conectar a su impresora. Intente mas tarde.');
+      $ionicLoading.hide();
+    });
+  }
+
+
+})
+
+
+.controller('MainCtrl', function($scope, $state, $localStorage, $location) {
+  
+
+})
+
+.controller('LoginCtrl', function($scope, $ionicPopup, $ionicLoading, $localStorage, $state, $location) {
+  $scope.user = {
+    username: '',
+    password : ''
+  };
+
+  if (!$localStorage.app) { $localStorage.app = app; }
+  else {
+    if ($localStorage.app.auth == 1) {
+      $location.path( "home/main", false );
+    }
+  }
+  
+
+  $scope.showload = function() {
+    $ionicLoading.show({
+      template: '<ion-spinner></ion-spinner>'
+    }).then(function(){
+       //console.log("The loading indicator is now displayed");
+    });
+  };
+  $scope.hideload = function(){
+    $ionicLoading.hide().then(function(){
+       //console.log("The loading indicator is now hidden");
+    });
+  };
+
+  $scope.signIn = function(form) {
+    //console.log(form);
+    if(form.$valid) {
+      $scope.showload();
+      jQuery.post(app.restApi+"action=validarInstalacion", {clave: $scope.user.password}, function(data) {
+        if (data.clave == 1) {
+          app.auth = 1;
+          $localStorage.app = app;
+          $location.path( "home/main", false );
+        }
+        else {
+          $ionicPopup.alert({
+            title: 'No autorizado',
+            content: 'La clave es inválida'
+          }).then(function(res) {
+            
+          });
+        }
+        $scope.hideload();
+
+      },"json");
+      
+    }
+  };
+});
